6ba8aef5ef88a140308b4682aab85f8f528ed818
Dylan Le <68560708+ExoticButters15@users.noreply.github.com>
2025-04-09 23:34:43 -0700

    main changed to app, and removed prompt resources

---
+++ server/app.py
@@ -0,0 +1,19 @@
+from google import genai
+from dotenv import find_dotenv, load_dotenv
+import os
+
+def main():
+    env_path = find_dotenv()
+    load_dotenv(env_path)
+    gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=gemini_api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents="Is tarragon and vanilla a good flavor combination for butter poached lobster?"
+    )
+
+    print(response.text)
+
+main()
